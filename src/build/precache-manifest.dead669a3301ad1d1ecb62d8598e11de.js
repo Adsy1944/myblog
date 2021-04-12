367a983df21e17e52f8e75afe3e9658d89f94c71
Adam <Crofts>
2021-04-12 10:58:25 +0100

    First commit

---
+++ src/build/precache-manifest.dead669a3301ad1d1ecb62d8598e11de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9eee5fc79b2ba59d34",
+    "url": "/static/css/main.de5cbb5b.chunk.css"
+  },
+  {
+    "revision": "2c9eee5fc79b2ba59d34",
+    "url": "/static/js/main.0e8873fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6fd623e6c1184df9f22a",
+    "url": "/static/js/2.78db25b5.chunk.js"
+  },
+  {
+    "revision": "f099f1c7ba7bce53044981d51f423bc1",
+    "url": "/index.html"
+  }
+];
